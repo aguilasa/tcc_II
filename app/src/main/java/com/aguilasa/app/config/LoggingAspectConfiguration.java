3bc4f55958fe595db78ebed28bc2af49420cdf9c
Ingmar Aguiar <ingmar.aguiar@gmail.com>
2019-09-23 21:53:36 -0300

    Initial version of app generated by JHipster-6.3.1

---
+++ app/src/main/java/com/aguilasa/app/config/LoggingAspectConfiguration.java
@@ -0,0 +1,19 @@
+package com.aguilasa.app.config;
+
+import com.aguilasa.app.aop.logging.LoggingAspect;
+
+import io.github.jhipster.config.JHipsterConstants;
+
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
