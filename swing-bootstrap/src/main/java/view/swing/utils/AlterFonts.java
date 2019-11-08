8317f653878dcc8262a76e8cf65599a29fe9b6f1
Ingmar Aguiar <ingmar.aguiar@senior.com.br>
2019-11-08 18:00:53 -0300

    Font

--- swing-bootstrap/src/main/java/view/swing/utils/AlterFonts.java
+++ swing-bootstrap/src/main/java/view/swing/utils/AlterFonts.java
@@ -7,7 +7,8 @@ import javax.swing.UIManager;
 public class AlterFonts {
 	public static final String DEFAULT_FONT_NAME = "SansSerif";
 	public static final String ALTERNATIVE_FONT_NAME = "HelveticaNeue";
-	public static final String FONT_NAME = ALTERNATIVE_FONT_NAME;
+	public static final String SEGOE = "Segoe UI Light";
+	public static final String FONT_NAME = DEFAULT_FONT_NAME;
 	
 	public static final Font DEFAULT_FONT = new Font(FONT_NAME, Font.TRUETYPE_FONT, 12);
 	public static final Font ALTERNATIVE_FONT = new Font(ALTERNATIVE_FONT_NAME, Font.TRUETYPE_FONT, 12);
